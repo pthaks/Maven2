37007ebb8965698bc746fefbbe943dcabd52021c
pthaks <priya.a.thakur@gmail.com>
2017-01-06 14:39:49 +0530

    First commit of maven project

---
+++ src/test/java/org/pthaks/Maven2/AppTest.java
@@ -0,0 +1,12 @@
+package org.pthaks.Maven2;
+
+import org.testng.annotations.Test;
+
+public class AppTest
+{
+	@Test
+	public void pfunc()
+	{
+		System.out.println("running testng program..........Priya");
+	}
+}
