63055e7dcdae7e2d0ab4fa07c0e71442febc6954
pthaks <priya.a.thakur@gmail.com>
2017-01-09 14:13:14 +0530

    selenium script modified

--- src/test/java/org/pthaks/Maven2/AppTest.java
+++ src/test/java/org/pthaks/Maven2/AppTest.java
@@ -1,12 +1,42 @@
 package org.pthaks.Maven2;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+
 
 public class AppTest
 {
 	@Test
 	public void pfunc()
 	{
-		System.out.println("running testng program..........Priya");
+		
+		String projectPath = System.getProperty("user.dir"); //+ "\" + "utility\geckodriver.exe";
+		String FFdriverPath = projectPath.replaceAll("\\\\", "//");
+		FFdriverPath = FFdriverPath + "//utility//geckodriver.exe";
+		
+		System.out.println("FFdriverPath - " + FFdriverPath);
+		
+		System.setProperty("webdriver.gecko.driver", FFdriverPath);
+		WebDriver driver = new FirefoxDriver();
+		
+		try
+		{
+			driver.get("http://www.google.com");
+			
+			//Assert.assertEquals(driver.getTitle(), "Not google");
+			
+			System.out.println(driver.getTitle());
+			
+			System.out.println("Google opened..........Priya");
+
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getStackTrace());
+		}
+				
+		driver.close();
+		driver.quit();
 	}
 }
